bd7e015d995d2e842c57e7d6e8d3f5648c69a0c8
Oleg Zhovtanskyi <o.zhovtanskyi@gmail.com>
2025-04-13 12:55:19 +0200

    Create API request

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -9,5 +9,64 @@ import Foundation
 
 /// Object that represent a singlet API Call
 final class RMRequest {
+    ///Api Constants
+    private struct Constants {
+        /// Base URL
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    /// Desire endpoint
+    private let endpoint: RMEndpoint
+    /// Path components for api, if any
+    private let pathComponents: Set<String>
+    /// Query arguments for api, if any
+    private let queryParameters: [URLQueryItem]
     
+    //MARK: - Constructed url for the api request in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value  = $0.value else {return nil}
+                return "\($0.name)=\(value) "
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    /// Computed & constructed API url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    ///Desire http method
+    public let httpMethod = "GET"
+    
+    // MARK: - Public
+    
+    /// Construct request
+    /// - Parameters:
+    ///   - endpoint: Target endpoint
+    ///   - pathComponents: Collection of path components
+    ///   - queryParameters: Collection of query parametres
+    public init(endpoint: RMEndpoint,
+                pathComponents: Set<String> = [],
+                queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
